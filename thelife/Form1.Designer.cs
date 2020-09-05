06c4b31126404e120dda6d8cba73e5c90fa085ec
Rewalon <rewalon@subreal-team.com>
2020-09-05 10:10:30 +0500

    refactoring

--- thelife/Form1.Designer.cs
+++ thelife/Form1.Designer.cs
@@ -30,27 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.checkBoxDiesByAge = new System.Windows.Forms.CheckBox();
-            this.nudMaxAge = new System.Windows.Forms.NumericUpDown();
-            this.nudMinAge = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.buttonStop = new System.Windows.Forms.Button();
-            this.buttonStart = new System.Windows.Forms.Button();
-            this.nudDensity = new System.Windows.Forms.NumericUpDown();
-            this.nudResolution = new System.Windows.Forms.NumericUpDown();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudResolution = new System.Windows.Forms.NumericUpDown();
+            this.nudDensity = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.nudMaxAge)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudMinAge)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -64,10 +58,6 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.checkBoxDiesByAge);
-            this.splitContainer1.Panel1.Controls.Add(this.nudMaxAge);
-            this.splitContainer1.Panel1.Controls.Add(this.nudMinAge);
-            this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.buttonStop);
             this.splitContainer1.Panel1.Controls.Add(this.buttonStart);
             this.splitContainer1.Panel1.Controls.Add(this.nudDensity);
@@ -82,86 +72,57 @@
             this.splitContainer1.SplitterDistance = 150;
             this.splitContainer1.TabIndex = 0;
             // 
-            // checkBoxDiesByAge
+            // pictureBox
             // 
-            this.checkBoxDiesByAge.AutoSize = true;
-            this.checkBoxDiesByAge.Checked = true;
-            this.checkBoxDiesByAge.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxDiesByAge.Location = new System.Drawing.Point(13, 104);
-            this.checkBoxDiesByAge.Name = "checkBoxDiesByAge";
-            this.checkBoxDiesByAge.Size = new System.Drawing.Size(80, 17);
-            this.checkBoxDiesByAge.TabIndex = 9;
-            this.checkBoxDiesByAge.Text = "dies by age";
-            this.checkBoxDiesByAge.UseVisualStyleBackColor = true;
-            this.checkBoxDiesByAge.CheckedChanged += new System.EventHandler(this.checkBoxDiesByAge_CheckedChanged);
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(920, 490);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
             // 
-            // nudMaxAge
+            // label1
             // 
-            this.nudMaxAge.Location = new System.Drawing.Point(84, 141);
-            this.nudMaxAge.Minimum = new decimal(new int[] {
-            2,
-            0,
-            0,
-            0});
-            this.nudMaxAge.Name = "nudMaxAge";
-            this.nudMaxAge.Size = new System.Drawing.Size(49, 20);
-            this.nudMaxAge.TabIndex = 8;
-            this.nudMaxAge.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.nudMaxAge.Value = new decimal(new int[] {
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resolution";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(10, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Density";
+            // 
+            // nudResolution
+            // 
+            this.nudResolution.Location = new System.Drawing.Point(13, 26);
+            this.nudResolution.Maximum = new decimal(new int[] {
             25,
             0,
             0,
             0});
-            // 
-            // nudMinAge
-            // 
-            this.nudMinAge.Location = new System.Drawing.Point(13, 141);
-            this.nudMinAge.Minimum = new decimal(new int[] {
+            this.nudResolution.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.nudMinAge.Name = "nudMinAge";
-            this.nudMinAge.Size = new System.Drawing.Size(49, 20);
-            this.nudMinAge.TabIndex = 7;
-            this.nudMinAge.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.nudMinAge.Value = new decimal(new int[] {
-            5,
+            this.nudResolution.Name = "nudResolution";
+            this.nudResolution.Size = new System.Drawing.Size(120, 20);
+            this.nudResolution.TabIndex = 2;
+            this.nudResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudResolution.Value = new decimal(new int[] {
+            3,
             0,
             0,
             0});
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(10, 122);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 16);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Time of life";
-            // 
-            // buttonStop
-            // 
-            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonStop.Location = new System.Drawing.Point(13, 206);
-            this.buttonStop.Name = "buttonStop";
-            this.buttonStop.Size = new System.Drawing.Size(120, 33);
-            this.buttonStop.TabIndex = 5;
-            this.buttonStop.Text = "Stop";
-            this.buttonStop.UseVisualStyleBackColor = true;
-            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
-            // 
-            // buttonStart
-            // 
-            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonStart.Location = new System.Drawing.Point(13, 167);
-            this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(120, 33);
-            this.buttonStart.TabIndex = 4;
-            this.buttonStart.Text = "Start";
-            this.buttonStart.UseVisualStyleBackColor = true;
-            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // nudDensity
             // 
@@ -181,58 +142,27 @@
             0,
             0});
             // 
-            // nudResolution
+            // buttonStart
             // 
-            this.nudResolution.Location = new System.Drawing.Point(13, 26);
-            this.nudResolution.Maximum = new decimal(new int[] {
-            25,
-            0,
-            0,
-            0});
-            this.nudResolution.Minimum = new decimal(new int[] {
-            2,
-            0,
-            0,
-            0});
-            this.nudResolution.Name = "nudResolution";
-            this.nudResolution.Size = new System.Drawing.Size(120, 20);
-            this.nudResolution.TabIndex = 2;
-            this.nudResolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.nudResolution.Value = new decimal(new int[] {
-            3,
-            0,
-            0,
-            0});
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(13, 113);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 33);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
-            // label2
+            // buttonStop
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(10, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Density";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(10, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Resolution";
-            // 
-            // pictureBox
-            // 
-            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(920, 490);
-            this.pictureBox.TabIndex = 0;
-            this.pictureBox.TabStop = false;
-            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStop.Location = new System.Drawing.Point(13, 152);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(120, 33);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
             // 
             // timer1
             // 
@@ -247,17 +177,14 @@
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.nudMaxAge)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudMinAge)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudResolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDensity)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -273,10 +200,6 @@
         private System.Windows.Forms.Button buttonStop;
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.NumericUpDown nudMaxAge;
-        private System.Windows.Forms.NumericUpDown nudMinAge;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.CheckBox checkBoxDiesByAge;
     }
 }
 
--- thelife/Form1.cs
+++ thelife/Form1.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,16 +14,11 @@ namespace thelife
     public partial class Form1 : Form
     {
         private Graphics graphics;
-        private int currentGeneration = 0;
+        private int currentGeneration = 0; 
         private int resolution;
         private bool[,] field;
-        private Brush[,] fieldColor;
-        private int[,] lifeTime;
         private int rows;
         private int cols;
-        private int minAge = 5;
-        private int maxAge = 10;
-        private int countLifes = 0;
 
         public Form1()
         {
@@ -34,33 +28,24 @@ namespace thelife
         {
             if (timer1.Enabled)
                 return;
-            
-            countLifes = 0;
-            currentGeneration = 0;
 
+            currentGeneration = 0;
             Text = $"Generation {currentGeneration}";
-            SetStatusControls(false);         
+            nudResolution.Enabled = false;
+            nudDensity.Enabled = false;
 
             resolution = (int)nudResolution.Value;
-
-            minAge = (int)nudMinAge.Value;
-            maxAge = (int)nudMaxAge.Value;
 
             rows = pictureBox.Height / resolution;
             cols = pictureBox.Width / resolution;
             field = new bool[cols, rows];
-            fieldColor = new Brush[cols, rows];
-            lifeTime = new int[cols, rows];
-            Random random = new Random();
 
+            Random random = new Random();
             for (int x = 0; x < cols; x++)
             {
                 for (int y = 0; y < rows; y++)
                 {
                     field[x, y] = random.Next((int)nudDensity.Value) == 0;
-                    if (field[x, y]) countLifes++;
-                    fieldColor[x, y] = PickBrush(9);
-                    lifeTime[x, y] = GetNewLifeTime();
                 }
             }
 
@@ -70,48 +55,11 @@ namespace thelife
             timer1.Start();
         }
 
-        private void SetStatusControls(bool status)
-        {
-            nudResolution.Enabled = status;
-            nudDensity.Enabled = status;
-            checkBoxDiesByAge.Enabled = status;
-            nudMinAge.Enabled = status && checkBoxDiesByAge.Checked;
-            nudMaxAge.Enabled = status && checkBoxDiesByAge.Checked;
-        }
-
-        private int GetNewLifeTime()
-        {
-            Random random = new Random();
-            return random.Next(minAge, maxAge);
-        }
-
-        private Brush PickBrush(int i)
-        {
-            if (i > 9) i = 9;
-            if (i < 0) i = 0;
-            Brush[] brushes = new Brush[] {
-                Brushes.DarkViolet,
-                Brushes.Violet,
-                Brushes.Red,
-                Brushes.Orange,
-                Brushes.Yellow,
-                Brushes.LightYellow,
-                Brushes.Green,
-                Brushes.LightGreen,
-                Brushes.Blue,
-                Brushes.LightBlue
-                };
-
-            return brushes[i];
-        }
-
         private void NextGeneration()
         {
-            countLifes = 0;
             graphics.Clear(Color.Black);
 
             var newField = new bool[cols, rows];
-            var newFieldColor = new Brush[cols, rows];
 
             for (int x = 0; x < cols; x++)
             {
@@ -119,55 +67,41 @@ namespace thelife
                 {
                     var neighboursCount = CountNeighbours(x, y);
                     var hasLife = field[x, y];
-                    // birth of a new life
+
                     if (!hasLife && neighboursCount == 3)
                     {
                         newField[x, y] = true;
-                        newFieldColor[x, y] = PickBrush(9);
-                        lifeTime[x, y] = GetNewLifeTime();
                     }
-                    // life dies
                     else if (hasLife && (neighboursCount < 2 || neighboursCount > 3))
                     {
                         newField[x, y] = false;
-                        newFieldColor[x, y] = PickBrush(0);
-                        lifeTime[x, y] = 0;
                     }
-                    // life continues to live
                     else
                     {
                         newField[x, y] = field[x, y];
-                        // life dies by age
-                        if (--lifeTime[x, y] <= 0)
-                        {
-                            if (checkBoxDiesByAge.Checked)
-                            {
-                                newField[x, y] = false;
-                                newFieldColor[x, y] = PickBrush(0);
-                            }
-                            lifeTime[x, y] = 0;
-                        }
-                        newFieldColor[x, y] = PickBrush(lifeTime[x, y]);                
                     }
 
                     if (hasLife)
                     {
-                        countLifes++;
-                        graphics.FillRectangle(fieldColor[x, y], x * resolution, y * resolution, resolution - 1, resolution - 1);
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution, resolution);
                     };
                 }
             }
 
             field = newField;
-            fieldColor = newFieldColor;
-
+            /*
+                        Random random = new Random();
+                        for (int x = 0; x < cols; x++)
+                        {
+                            for (int y = 0; y < rows; y++)
+                            {
+                                field[x, y] = random.Next((int)nudDensity.Value) == 0;
+                            }
+                        }
+            */
             pictureBox.Refresh();
-            ShowStatistic();
-        }
+            Text = $"Generation {++currentGeneration}";
 
-        private void ShowStatistic()
-        {
-            Text = $"Generation {++currentGeneration}, Lifes {countLifes}";
         }
 
         private int CountNeighbours(int x, int y)
@@ -186,39 +120,23 @@ namespace thelife
                     {
                         count++;
                     }
-                }
-            }
-            return count;
-        }
 
-        private bool isThereLife()
-        {
-            for (int x = 0; x < cols; x++)
-            {
-                for (int y = 0; y < rows; y++)
-                {
-                    if (field[x,y]) { return true; }
                 }
             }
-            return false;
+
+
+            return count;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (isThereLife()) 
-            {
-                NextGeneration();
-            }
-            else
-            {
-                Text = $"Generation {currentGeneration} - a Life is DEAD!";
-                StopGame();
-            }
+            NextGeneration();
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
         {
             StartGame();
+            //graphics.FillRectangle(Brushes.Crimson, 0, 0, resolution, resolution);
         }
 
         private void buttonStop_Click(object sender, EventArgs e)
@@ -232,49 +150,8 @@ namespace thelife
                 return;
 
             timer1.Stop();
-            SetStatusControls(true);
-        }
-
-        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (!timer1.Enabled)
-                return;
-
-            if (e.Button == MouseButtons.Left)
-            {
-                var x = e.Location.X / resolution;
-                var y = e.Location.Y / resolution;
-                var validationPassed = ValidateMousePosition(x, y);
-                if (validationPassed)
-                {
-                    field[x, y] = true;
-                }
-            }
-
-            if (e.Button == MouseButtons.Right)
-            {
-                var x = e.Location.X / resolution;
-                var y = e.Location.Y / resolution;
-                var validationPassed = ValidateMousePosition(x, y);
-                if (validationPassed)
-                    field[x, y] = false;
-            }
-        }
-
-        private bool ValidateMousePosition(int x, int y)
-        {
-            return x >= 0 && y >= 0 && x < cols && y < rows;
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            ShowStatistic();
-        }
-
-        private void checkBoxDiesByAge_CheckedChanged(object sender, EventArgs e)
-        {
-            nudMinAge.Enabled = checkBoxDiesByAge.Checked;
-            nudMaxAge.Enabled = checkBoxDiesByAge.Checked;
+            nudResolution.Enabled = true;
+            nudDensity.Enabled = true;
         }
     }
 }
